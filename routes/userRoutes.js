7cf697e1d715adcf8c300a1419ef46ab490e7a34
shahzaib <muhammadshahzaibqureshi497@gmail.com>
2024-08-23 11:11:15 +0500

    validation added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,6 +17,11 @@ router.get('/', (req, res) => {
 router.post('/register', async (req, res) => {
     try {
         const { name, email, password } = req.body;
+         // Check if the email already exists
+         const existingUser = await User.findOne({ email });
+         if (existingUser) {
+             return res.status(400).send({ error: "Email is already taken", status: false });
+         }
         const user = new User({ name, email, password });
         await user.save();
         res.status(201).send({ user, message: "user created successfully" });
